ec3db782f9608b8ab210dce3c09468db9b6eeb8e
Taras Kadlub <taras.kadlub988@gmail.com>
2020-03-06 22:44:04 +0200

    Updates

---
+++ precache-manifest.3aa1de4b20007dafe0ed9f92e9ca2f6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b0cb67fa425d3dd504c360d5b4fe90",
+    "url": "/Hd_Rezka/index.html"
+  },
+  {
+    "revision": "2264be5d1906734f33f9",
+    "url": "/Hd_Rezka/static/css/main.5cb9bdeb.chunk.css"
+  },
+  {
+    "revision": "7afa43e42cfb9cc4bddc",
+    "url": "/Hd_Rezka/static/js/2.de22f665.chunk.js"
+  },
+  {
+    "revision": "2264be5d1906734f33f9",
+    "url": "/Hd_Rezka/static/js/main.5ed38403.chunk.js"
+  },
+  {
+    "revision": "c93e4b0fad7c0f1996b7",
+    "url": "/Hd_Rezka/static/js/runtime-main.50ef314d.js"
+  },
+  {
+    "revision": "585b1b43369c9cb8424bc4fafa44fe4e",
+    "url": "/Hd_Rezka/static/media/Sgdo_u2j.585b1b43.png"
+  },
+  {
+    "revision": "661fffda6bc8f6b2b7ba4305af7e3e33",
+    "url": "/Hd_Rezka/static/media/icon.661fffda.png"
+  },
+  {
+    "revision": "645a9dafe4a65786d88199568652037e",
+    "url": "/Hd_Rezka/static/media/img.645a9daf.png"
+  }
+]);
